363fc37c2332a786f8719bf2d2a5257fc3796ee4
luis andia <luisandiamamani@gmail.com>
2020-05-03 01:57:18 -0500

    added redux thunk

--- react-redux-ts/src/redux/user-events.ts
+++ react-redux-ts/src/redux/user-events.ts
@@ -1,4 +1,6 @@
-import { AnyAction } from 'redux';
+import { AnyAction, Action } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import { RootState } from './store';
 
 interface UserEvent {
   id: number;
@@ -12,16 +14,71 @@ interface UserEventsState {
   allIds: UserEvent['id'][];
 }
 
+const LOAD_REQUEST = 'userEvents/load_request';
+
+interface LoadReaquestAction extends Action<typeof LOAD_REQUEST> {}
+
+const LOAD_SUCCESS = 'userEvents/load_success';
+
+interface LoadSuccessAction extends Action<typeof LOAD_SUCCESS> {
+  payload: {
+    events: UserEvent[];
+  };
+}
+
+const LOAD_FAILURE = 'userEvents/load_failure';
+
+interface LoadFailureAction extends Action<typeof LOAD_FAILURE> {
+  error: string;
+}
+
+export const loadUserEvents = (): ThunkAction<
+  void, // return value
+  RootState, // rootstate
+  undefined, // extra arguments
+  LoadReaquestAction | LoadSuccessAction | LoadFailureAction // actions
+> => async (dispatch, getState) => {
+  dispatch({
+    type: LOAD_REQUEST,
+  });
+
+  try {
+    const response = await fetch('http://localhost:3001/events');
+    const events: UserEvent[] = await response.json();
+
+    dispatch({
+      type: LOAD_SUCCESS,
+      payload: { events },
+    });
+  } catch (e) {
+    dispatch({
+      type: LOAD_FAILURE,
+      error: 'Failed to load events.',
+    });
+  }
+};
+
 const initialState: UserEventsState = {
   byIds: {},
-  allIds: []
+  allIds: [],
 };
 
 const userEventsReducer = (
   state: UserEventsState = initialState,
-  action: AnyAction
+  action: LoadSuccessAction,
 ) => {
   switch (action.type) {
+    case LOAD_SUCCESS:
+      const { events } = action.payload;
+      return {
+        ...state,
+        allIds: events.map(({ id }) => id),
+        byIds: events.reduce<UserEventsState['byIds']>((byIds, event) => {
+          byIds[event.id] = event;
+          return byIds;
+        }, {}),
+      };
+
     default:
       return state;
   }
