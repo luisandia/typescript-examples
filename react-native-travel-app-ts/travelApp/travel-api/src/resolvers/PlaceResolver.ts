7104636837a0639462e3b3da4e1b5fb8fe15f4dd
luis andia <luisandiamamani@gmail.com>
2020-05-12 23:31:08 -0500

    added codegen config and graphql queries

--- react-native-travel-app-ts/travelApp/travel-api/src/resolvers/PlaceResolver.ts
+++ react-native-travel-app-ts/travelApp/travel-api/src/resolvers/PlaceResolver.ts
@@ -6,11 +6,13 @@ import { PlaceInput } from "../graphql-types/PlaceInput";
 
 @Resolver(() => Place)
 export class PlaceResolver {
+  // Returns a single place based on it's ID, or undefined.
   @Query(() => Place, { nullable: true })
   async place(@Arg("id") id: number): Promise<Place | undefined> {
     return await Place.findOne(id);
   }
 
+  // Returns a list of all places, or an empty array.
   @Query(() => [Place], {
     description: "Get all the places from around the world ",
   })
@@ -18,7 +20,7 @@ export class PlaceResolver {
     const places = await Place.find();
     return places;
   }
-
+  // Adds and returns a new place using PlaceInput parameters.
   @Mutation(() => Place)
   async createPlace(@Arg("place") placeInput: PlaceInput): Promise<Place> {
     const place = plainToClass(Place, {
